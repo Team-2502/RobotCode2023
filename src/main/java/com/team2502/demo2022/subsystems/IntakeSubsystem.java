113a5d1424b33f291a337de895d399d683561148
isaacmyers11 <90308039@ep-student.org>
2023-02-09 18:19:33 -0600

    Added motors to subsystem

--- src/main/java/com/team2502/demo2022/subsystems/IntakeSubsystem.java
+++ src/main/java/com/team2502/demo2022/subsystems/IntakeSubsystem.java
@@ -1,4 +1,19 @@
 package com.team2502.demo2022.subsystems;
 
-public class IntakeSubsystem {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.team2502.demo2022.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private final CANSparkMax liftMotor;
+    private final CANSparkMax rightMotor;
+    private final CANSparkMax leftMotor;
+
+
+    public IntakeSubsystem() {
+        liftMotor = new CANSparkMax(Constants.HardwareMap.LIFT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+        rightMotor = new CANSparkMax(Constants.HardwareMap.RIGHT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+        leftMotor = new CANSparkMax(Constants.HardwareMap.LEFT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+    }
 }
