785bc6302a355d576557d273ce8c21684fc9016f
isaacmyers11 <90308039@ep-student.org>
2023-02-09 18:23:52 -0600

    Limits, Inverted, and Comments

--- src/main/java/com/team2502/demo2022/subsystems/IntakeSubsystem.java
+++ src/main/java/com/team2502/demo2022/subsystems/IntakeSubsystem.java
@@ -6,14 +6,25 @@ import com.team2502.demo2022.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
+    //Define motors
     private final CANSparkMax liftMotor;
     private final CANSparkMax rightMotor;
     private final CANSparkMax leftMotor;
 
 
     public IntakeSubsystem() {
+        // Pulls Motor Info From Constants and Defines Motor Type
         liftMotor = new CANSparkMax(Constants.HardwareMap.LIFT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
         rightMotor = new CANSparkMax(Constants.HardwareMap.RIGHT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
         leftMotor = new CANSparkMax(Constants.HardwareMap.LEFT_INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        //Sets Speed Limit To Motors
+        //TODO CHANGE SPEED!!!
+        liftMotor.setSmartCurrentLimit(20);
+        rightMotor.setSmartCurrentLimit(20);
+        leftMotor.setSmartCurrentLimit(20);
+
+        //Sets Left Motor To Spin Opposite Direction To Work With Right Motor
+        leftMotor.setInverted(true);
     }
 }
