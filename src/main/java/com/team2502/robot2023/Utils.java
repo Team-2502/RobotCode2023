3236909192dfd992a18bd3a9a9e13f9c72cf4d94
Sha-dos <77746688+Sha-dos@users.noreply.github.com>
2023-02-16 18:26:13 -0600

    added constrain and friction adjust utils

--- src/main/java/com/team2502/robot2023/Utils.java
+++ src/main/java/com/team2502/robot2023/Utils.java
@@ -6,6 +6,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Utils {
     /**
+     * Constrain val between -max and max
+     * @param val value to constrain
+     * @param max maximum and minimum value
+     * @return constrained value
+     */
+    public static double constrain(double val, double max) {
+        return Math.max(-max,Math.min(max,val));
+    }
+
+    /**
+     * Adjust value so (|val| >= friction)
+     * @param val value to adjust
+     * @param friction minimum
+     * @return adjusted value
+     */
+    public static double frictionAdjust(double val, double friction) {
+        double frictionVal = val < friction ? friction : 0;
+
+        if (val > 0) {
+            return val +  frictionVal;
+        } else {
+            return val - frictionVal;
+        }
+    }
+    /**
      * Minimize the change in heading the desired swerve module state would require by potentially
      * reversing the direction the wheel spins. Customized from WPILib's version to include placing
      * in appropriate scope for CTRE onboard control.
