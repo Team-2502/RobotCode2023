f0c1878953505dd3c83d835355238ad88efa7a76
64001830 <ndpete2007@gmail.com>
2023-02-21 13:31:10 -0600

    added limit switch to Elevator

--- src/main/java/com/team2502/robot2023/subsystems/ElevatorSubsystem.java
+++ src/main/java/com/team2502/robot2023/subsystems/ElevatorSubsystem.java
@@ -7,6 +7,7 @@ import com.team2502.robot2023.Constants;
 import com.team2502.robot2023.Constants.Subsystems.Elevator.ElevatorPitch;
 import com.team2502.robot2023.Constants.Subsystems.Elevator.ElevatorPosition;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,6 +15,8 @@ public class ElevatorSubsystem extends SubsystemBase {
     private CANSparkMax leftElevator;
     private CANSparkMax rightElevator;
     private CANSparkMax pitchElevator;
+
+    private DigitalInput limitSwitch;
 
     public ElevatorSubsystem() {
         leftElevator = new CANSparkMax(Constants.HardwareMap.LEFT_ELEVATOR_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -31,6 +34,8 @@ public class ElevatorSubsystem extends SubsystemBase {
         rightElevator.setSoftLimit(SoftLimitDirection.kReverse,(float) Constants.Subsystems.Elevator.ELEVATOR_LIM_TOP);
         rightElevator.enableSoftLimit(SoftLimitDirection.kForward,true);
         rightElevator.enableSoftLimit(SoftLimitDirection.kReverse,true);
+
+        limitSwitch = new DigitalInput(Constants.HardwareMap.SWITCH_ELEVATOR);
     }
 
     @Override
@@ -52,6 +57,14 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     public void setPitchSpeed(double speed) {
         pitchElevator.set(speed);
+    }
+
+    public void home() {
+        while (limitSwitch.get()) {
+            setLinearSpeed(-0.1);
+        }
+
+        set(ElevatorPosition.BOTTOM);
     }
 
     public void stop() {
