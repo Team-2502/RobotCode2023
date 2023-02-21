c08cd48ca57fb25a4df61c875e9f92479a2eb528
64001830 <ndpete2007@gmail.com>
2023-02-21 13:34:05 -0600

    added limit switch to Manipulator

--- src/main/java/com/team2502/robot2023/subsystems/ManipulatorSubsystem.java
+++ src/main/java/com/team2502/robot2023/subsystems/ManipulatorSubsystem.java
@@ -3,10 +3,13 @@ package com.team2502.robot2023.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.team2502.robot2023.Constants;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ManipulatorSubsystem extends SubsystemBase {
     private CANSparkMax gripper;
+
+    private DigitalInput limitSwitch;
 
     public ManipulatorSubsystem() {
         gripper = new CANSparkMax(Constants.HardwareMap.GRIPPER_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -18,6 +21,12 @@ public class ManipulatorSubsystem extends SubsystemBase {
 
     public void setSpeed(double speed) {
         gripper.set(speed);
+    }
+
+    public void home() {
+        while (limitSwitch.get()) {
+            setSpeed(-0.1);
+        }
     }
 
     public void stop() {
