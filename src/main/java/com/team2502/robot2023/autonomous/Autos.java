3092d25e5a18105e32bf4deb80e5861d8dc9cdcc
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2023-02-24 16:27:10 -0600

    added test auto

--- src/main/java/com/team2502/robot2023/autonomous/Autos.java
+++ src/main/java/com/team2502/robot2023/autonomous/Autos.java
@@ -1,19 +1,24 @@
 package com.team2502.robot2023.autonomous;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import static edu.wpi.first.wpilibj2.command.CommandGroupBase.*;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 import com.team2502.robot2023.autonomous.AutoChooser.CommandFactory;
+import com.team2502.robot2023.commands.FollowPathAbsoluteCommand;
 
 /**
  * class for autonomous command groups
  * put new groups before the do nothing group
  * */
 public enum Autos { // first auto is default
+        TEST_PATH((d,i) -> Commands.sequence(
+            new InstantCommand(d::resetHeading),
+            new InstantCommand(() -> d.setPose(new Pose2d(14.693,4.678,Rotation2d.fromDegrees(180))), d),
+            new FollowPathAbsoluteCommand(d, "testpath")
+        )), // always put last
+         
         DO_NOTHING("Do Nothing", ((d,i) -> new InstantCommand(d::resetHeading))); // always put last
 
         Autos(String name, AutoChooser.CommandFactory commandFactory)
